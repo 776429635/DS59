9121d7785d1e88d74891bb59cd27e30d39f5d3d2
lenovopc <lenovopc@192.168.1.123>
2017-12-20 19:58:21 +0800

    123

--- src/main/java/dt59homework/zsx.java
+++ src/main/java/dt59homework/zsx.java
@@ -1,20 +1,20 @@
 /**
  * Project Name:dt59homework
- * File Name:zxc.java
+ * File Name:zsx.java
  * Package Name:dt59homework
- * Date:2017年12月20日下午7:54:43
+ * Date:2017年12月20日下午7:57:58
  * Copyright (c) 2017, bluemobi All Rights Reserved.
  */
 
 package dt59homework;
 /**
  * Description:   <br/>
- * Date:     2017年12月20日 下午7:54:43 <br/>
+ * Date:     2017年12月20日 下午7:57:58 <br/>
  * @author   lenovopc
  * @version
  * @see
  */
-public class zxc {
+public class zsx {
 
     public static void main(String[] args) {
 
